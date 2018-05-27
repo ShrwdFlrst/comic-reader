41fd16566e4477c90db543ec8c808022714b808c
Matt <shrwdflrst@gmail.com>
2018-05-27 13:12:12 +0100

    UPDATE copy dropped file to temp dir

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const electron = require('electron')
+const temp = require('temp').track()
+
+const TMP_DIR_PREFIX = 'com.shrwdflrst.Comic'
 // Module to control application life.
 const app = electron.app
+const ipcRenderer = electron.ipcRenderer
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
@@ -11,27 +15,28 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+    init()
 }
 
 // This method will be called when Electron has finished
@@ -41,20 +46,34 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+    cleanUp()
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+let tempDir
+
+function init() {
+    tempDir = temp.mkdirSync(TMP_DIR_PREFIX)
+    global.sharedObj = {tempDir: tempDir};
+    console.log('Created temp dir: ' + tempDir)
+}
+
+function cleanUp() {
+    console.log('clean up')
+    console.log(temp.cleanupSync())
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,42 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+// window.$ = window.jQuery = require('jquery')
+const fs = require('fs')
+const path = require('path')
+const remote = require('electron').remote
+const tempDir = remote.getGlobal('sharedObj').tempDir
+
+console.log(tempDir);
+
+document.ondragover = document.ondrop = (ev) => {
+    ev.preventDefault()
+}
+
+document.body.ondrop = (ev) => {
+    let file = ev.dataTransfer.files[0].path;
+    let name = ev.dataTransfer.files[0].name;
+    ev.preventDefault()
+    let info = fs.statSync(file);
+    console.log(info)
+    fs.copyFileSync(file, path.join(tempDir, name))
+}
+//
+// $("html").on("dragover", function(event) {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     // console.log('drag over');
+// });
+//
+// $("html").on("dragleave", function(event) {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     // console.log('drag leave');
+// });
+//
+// $("html").on("drop", function(event) {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     console.log(event.dataTransfer.files[0].path)
+//     console.log('dropped');
+// });
